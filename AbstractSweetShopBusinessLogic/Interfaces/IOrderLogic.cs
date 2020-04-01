6dbda6e3d832d000750bbc946d05e5ec0b2352f2
Vikabraunone <vikabraunone@gmail.com>
2020-04-01 22:03:07 +0400

    Lab 2 ver 2

--- AbstractSweetShopBusinessLogic/Interfaces/IOrderLogic.cs
+++ AbstractSweetShopBusinessLogic/Interfaces/IOrderLogic.cs
@@ -7,7 +7,9 @@ namespace AbstractSweetShopBusinessLogic.Interfaces
     public interface IOrderLogic
     {
         List<OrderViewModel> Read(OrderBindingModel model);
+
         void CreateOrUpdate(OrderBindingModel model);
+
         void Delete(OrderBindingModel model);
     }
 }
