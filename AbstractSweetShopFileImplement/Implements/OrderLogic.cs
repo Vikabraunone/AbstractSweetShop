a0f5b2e76c6744678da5d4f556b80dd352a737af
Vikabraunone <vikabraunone@gmail.com>
2020-05-06 12:15:58 +0400

    Lab 2 ver 5

--- AbstractSweetShopFileImplement/Implements/OrderLogic.cs
+++ AbstractSweetShopFileImplement/Implements/OrderLogic.cs
@@ -59,7 +59,7 @@ namespace AbstractSweetShopFileImplement.Implements
                     Sum = rec.Sum,
                     Status = rec.Status,
                     ProductId = rec.ProductId,
-                    ProductName = source.Products.FirstOrDefault(rec1 => rec.ProductId == rec1.Id).ProductName,
+                    ProductName = source.Products.FirstOrDefault(rec1 => rec.ProductId == rec1.Id)?.ProductName,
                     DateCreate = rec.DateCreate,
                     DateImplement = rec.DateImplement
                 })
